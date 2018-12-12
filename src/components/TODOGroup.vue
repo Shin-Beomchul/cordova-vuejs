7d6ed52048b0f81c736505777a017db035cad7f1
GodBeom <sbc0830@osstem.com>
2018-12-12 17:45:40 +0900

    complete group tree view(dirty)

--- src/components/TODOGroup.vue
+++ src/components/TODOGroup.vue
@@ -2,8 +2,8 @@
 <template>
   <div>
     <tree
-    class="item"
-    :model="treeData">
+      class="item"
+      :model=tree>
     </tree>
   </div>
 
@@ -16,7 +16,69 @@ export default {
 
   data () {
     return {
+      tree: {},
+      treeData2: [
 
+        {
+
+          name: 'My Tree',
+          children: [
+            {
+
+              name: 'child folder',
+              children: [
+                {name: 'hello'},
+                {name: 'wat'}
+              ]
+            },
+            {
+
+              name: 'child folder',
+              children: [
+                {name: 'hello'},
+                {name: 'wat'}
+              ]
+            },
+            {
+
+              name: 'child folder',
+              children: [
+                {name: 'hello'},
+                {name: 'wat'}
+              ]
+            }
+          ]
+        },
+        {
+
+          name: 'My Tree',
+          children: [
+            {
+
+              name: 'child folder',
+              children: [
+                {name: 'hello'},
+                {name: 'wat'}
+              ]
+            },
+            {
+
+              name: 'child folder',
+              children: [
+                {name: 'hello'},
+                {name: 'wat'}
+              ]
+            },
+            {
+
+              name: 'child folder',
+              children: [
+                {name: 'hello'},
+                {name: 'wat'}
+              ]
+            }
+          ]
+        }],
       treeData: {
         isOpen: true,
         name: 'My Tree',
@@ -52,14 +114,149 @@ export default {
     }
   },
   methods: {
+    buildMyTree (resp) {
+      let tree = {}
+
+      tree.name = { id: '2',
+        isOpen: true,
+        orgName: '오스템임플란트(주)',
+        orgUpperCd: '2'}
+
+      tree.children = this.buildHierarchy(resp)
+      this.tree = tree
+    },
+
+    buildHierarchy (resp) {
+      let orgInfoList = resp.orgInfoList
+      let orgMemberInfoList = resp.orgMemberInfoList
+      var roots = [], children = {}, tree = {}
+
+      // 최상위 노드를 찾고 부모를 기반으로 자식을 해시합니다.
+      for (var i = 0, len = orgInfoList.length; i < len; ++i) {
+        var orginfo = orgInfoList[i]
+        var upperCd = orginfo.orgUpperCd // 부모가 있냐
+        var target = upperCd === '0000' ? roots //  id가 0000이면 target은 최상위 root
+          : (children[upperCd] || (children[upperCd] = []))
+        target.push({ name: orginfo })
+      }
+
+      // 재귀 적으로 트리를 만드는 함수
+      var findChildren = function (parent) {
+        if (children[parent.name.orgCd]) { // 자식이 있으면
+          parent.children = children[parent.name.orgCd]
+          console.log(parent.name.orgName + '++++')
+          for (var i = 0, len = parent.children.length; i < len; ++i) {
+            findChildren(parent.children[i])
+          }
+        } else {
+          console.log(parent.name.orgName + 'end + ')
+
+          let endNodeChilds = orgMemberInfoList.filter(e => e.orgCd === parent.name.orgCd)
+
+          for (var ie = 0, lenE = endNodeChilds.length; ie < lenE; ++ie) {
+            endNodeChilds[ie].isEndNode = true
+          }
+          parent.children = endNodeChilds
+
+          console.log(parent.children)
+        }
+      }
+
+      // root 탐색 시작.
+      for (var i = 0, len = roots.length; i < len; ++i) {
+        findChildren(roots[i])
+      }
+
+      return roots
+    },
     loadGnb () {
-      this.$http.get('http://192.168.22.94:10080/api/orginfo/member/nodes/OSSTEM/0000', {
+      this.$http.get('http://13.209.102.254:10080/api/orginfo/member/nodes/OSSTEM/0000', {
       })
         .then((result) => {
           console.log('source :: ')
           console.log(result.data)
-          this.apiData = result.data
+
+          this.buildMyTree(result.data)
         })
+    },
+
+    makeTreeSet2 (orgCd, orgInfoList, orgMemberInfoList) {
+      var resultTreeData = {}
+
+      resultTreeData.name = 'osstem조직'
+      resultTreeData.isOpen = true
+
+      resultTreeData.children = []
+
+      let parent = this.findByGroupUpperCd(orgCd, orgInfoList)
+      for (let pGroup of parent) {
+        var treeItem = {}
+        var children = []
+        treeItem.name = pGroup.orgName
+        treeItem.orgCd = pGroup.orgCd
+        treeItem.isOpen = false
+        console.log(pGroup.orgName + ' ' + pGroup.orgCd + ' [+]')
+
+        let childItems = this.findByGroupUpperCd(pGroup.orgCd, orgInfoList)
+        if (childItems.length > 0) {
+          for (const child of childItems) {
+            children.push({
+              isOpen: true,
+              name: child.orgName + '[+]',
+              orgCd: child.orgCd
+            })
+            treeItem.children = children
+            console.log('     =>' + child.orgName + ' ' + child.orgCd + '[+]')
+          }
+        } else {
+          let users = this.findByUserUpperCd(pGroup.orgCd, orgMemberInfoList)
+          for (let user of users) {
+            children.push({
+              isOpen: true,
+              name: user.name + '[ c]'
+            })
+            treeItem.children = children
+            console.log(user.name)
+          }
+        }
+        resultTreeData.children.push(treeItem)
+      }
+
+      console.log(resultTreeData)
+      this.tree = resultTreeData
+    },
+
+    makeTreeSet (orgCd, orgInfoList, orgMemberInfoList) {
+      let parent = this.findByGroupUpperCd(orgCd, orgInfoList)
+      for (let pGroup of parent) {
+        console.log(pGroup.orgName + ' ' + pGroup.orgCd + ' [+]')
+
+        let childItems = this.findByGroupUpperCd(pGroup.orgCd, orgInfoList)
+        if (childItems.length > 0) {
+          for (const child of childItems) {
+            console.log(child.orgName + ' ' + child.orgCd + '[ c]')
+            this.makeTreeSet(child.orgCd, orgInfoList, orgMemberInfoList)
+          }
+        } else {
+          let users = this.findByUserUpperCd(pGroup.orgCd, orgMemberInfoList)
+          for (let user of users) {
+            console.log(user.name)
+            this.makeTreeSet(user.orgCd, orgInfoList, orgMemberInfoList)
+          }
+        }
+      }
+    },
+    findByGroupUpperCd (orgCd, orgInfoList) {
+      return orgInfoList.filter(e => e.orgUpperCd === orgCd)
+    },
+    findByUserUpperCd (orgCd, orgMemberInfoList) {
+      return orgMemberInfoList.filter(e => e.orgCd === orgCd)
+    },
+    findByGroupOrgLevel (orgLevel, orgInfoList) {
+      return orgInfoList.filter(e => e.orgLevel === orgLevel)
+    },
+    findByGroupOrgCd (orgLevel, orgInfoList) {
+      return orgInfoList.filter(e => e.orgUpperCd === orgLevel)
     },
 
     // let nextGroupOrCdGroup = this.groupBy(nextGroups, org => org.orgCd)
--- src/components/webget/item.vue
+++ src/components/webget/item.vue
@@ -4,19 +4,27 @@
     <div
       :class="{bold: isFolder}"
       @click="toggle">
-      {{model.name}}
-      <span v-if="isFolder">[{{model.isOpen ? '-' : '+'}}]</span>
+      {{model.name.orgName}}
+      <span v-if="isFolder">[{{isOpen ? '-' : '+'}}]</span>
     </div>
+
     <!--childs-->
-    <ul v-show="model.isOpen" v-if="isFolder">
-        <tree
+    <ul v-show="isOpen" v-if="isFolder">
+      <tree
         class="item"
         v-for="model in model.children"
         :model="model">
       </tree>
 
+      <!--End Node-->
+      <div
+        class="item"
+        v-for="model in model.children"
+        :model="model">
+        <div v-if="model.isEndNode">{{model.name}}</div>
+      </div>
       <!--<li class="add" @click="addChild">+</li>-->
-      <li class="add" @click="addChildGroup">+g+</li>
+      <!--<li class="add" @click="addChildGroup">+g+</li>-->
     </ul>
   </div>
 </template>
@@ -29,7 +37,7 @@ export default {
   },
   data: function () {
     return {
-      open: true
+      isOpen: false
     }
   },
 
@@ -43,15 +51,16 @@ export default {
 
     toggle: function () {
       if (this.isFolder) {
-        alert('group ' + this.model.name)
-        this.model.isOpen = !this.model.isOpen
-      }else{
-        alert('just ' + this.model.name)
+        this.isOpen = !this.isOpen
       }
     },
     addChild: function () {
+      console.log(this.model)
       this.model.children.push({
-        name: 'new stuff'
+        orgCd: 'O000000691',
+        isOpen: true,
+        orgName: 'root',
+        orgUpperCd: 'O000000547'
       })
     },
     addChildGroup: function () {
@@ -79,7 +88,26 @@ export default {
           }
         ]
       })
-    }
+    },
+
+    togglesave: function () {
+      if (this.isFolder) {
+        this.model.name.isOpen = !this.model.name.isOpen
+      }
+
+      console.log(this.model.name.name + ' : ' + this.model.name.orgCd)
+
+      if (this.model.children === undefined) {
+        this.model.children = []
+        this.model.children.push({
+          isOpen: true,
+          name: 'new stuff + [ c]'
+        })
+        console.log('없음 child 추가 api call')
+      } else {
+        // group Click
+      }
+    },
   }
 }
 </script>
